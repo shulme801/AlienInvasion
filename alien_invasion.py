38322e488e2080d98b733147b5d9edfcf29000f7
shulme801 <shulme801@gmail.com>
2021-03-21 13:56:55 -0400

    21 March Add ability to start game with p key

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,6 @@
-""" This is my version of Eric Matthes' Alien Invasion game, from his book "Python Crash Course"
+"""
+This is my version of Eric Matthes Alien Invasion game,
+from his book Python Crash Course.
 """
 
 import sys
@@ -14,18 +16,19 @@ from ship import Ship
 from bullet import Bullet
 from alien import Alien
 
+
 class AlienInvasion:
     """Base/overall class to manage game assets and behavior
     """
 
     def __init__(self):
-        """Initialize the game and create game resources
+        """ Initialize the game and create game resources
         """
         pygame.init()
-
         self.settings = Settings()
 
-        # The following sets the screen width and height to the values defined in settings.py
+        # The following sets the screen width and height to the values 
+        # defined in settings.py
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
 
@@ -50,9 +53,34 @@ class AlienInvasion:
         self.fps = self.settings.fps
         self.fpsClock = pygame.time.Clock()
 
+    def _start_game(self):
+        """ This is the processing required to start a new game """
+
+        # Reset the game statistics.
+        self.stats.reset_stats()
+        self.stats.game_active = True
+
+        # Get rid of any remaining aliens and bullets.
+        self.aliens.empty()
+        self.bullets.empty()
+
+        # Create a new fleet and center the ship.
+        self._create_fleet()
+        self.ship.center_ship()
+
+    def _create_alien(self, alien_number, row_number):
+        """ Create a single alien. Place it in the row."""
+
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + (2 * alien_width * alien_number)
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + (2 * alien.rect.height * row_number)
+        self.aliens.add(alien)
 
     def _create_fleet(self):
         """ Create a fleet of aliens """
+
         # make an alien.
         alien = Alien(self)
         alien_width, alien_height = alien.rect.size
@@ -70,31 +98,26 @@ class AlienInvasion:
             for alien_number in range(number_aliens_x):
                 self._create_alien(alien_number, row_number)
 
-    def _create_alien(self, alien_number, row_number):
-        """ Create an alien. Place it in the row."""
-        alien = Alien(self)
-        alien_width, alien_height = alien.rect.size
-        alien.x = alien_width + (2 * alien_width * alien_number)
-        alien.rect.x = alien.x
-        alien.rect.y = alien.rect.height + \
-                       (2 * alien.rect.height * row_number)
-        self.aliens.add(alien)
-
     def _check_fleet_edges(self):
         """ Respond appropriately if any aliens have reached an edge."""
+
         for alien in self.aliens.sprites():
             if alien.check_edges():
                 self._change_fleet_direction()
                 break
 
     def _change_fleet_direction(self):
-        """ Drop the entire fleet and change the fleet's direction."""
+        """ Drop the entire alien fleet down the screen
+             and change the fleet's direction."""
+
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
 
+    # Keyboard and Play button handlers
     def _check_events(self):
         """ Respond to key presses and mouse events."""
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -108,43 +131,44 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """ Start a new game when the player clicks 'Play'."""
+        
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
-        if button_clicked and not self.stats.game_active:
-            # Reset the game statistics.
-            self.stats.reset_stats()
-            self.stats.game_active = True
-
-            # Get rid of any remaining aliens and bullets.
-            self.aliens.empty()
-            self.bullets.empty()
-
-            # Create a new fleet and center the ship.
-            self._create_fleet()
-            self.ship.center_ship()
-
+        if ((button_clicked) and (not self.stats.game_active):
+            self._start_game()    
             # Hide the mouse cursor.
             pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
-        """ Respond to key presses
-        """
+        """ Respond to key presses"""
+
+        # Note that we simply reset the 
+        # moving_rignt or moving_left flags
+        # when the arrow keys are pressed.
+        # the actual motion of our ship happens
+        # when the arrow keys are released.
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-        elif event.key == pygame.K_q:
-            sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
+        elif event.key == pygame.K_q: # Stop the game.
+            sys.exit()
+        elif ((event.key == pygame.K_p) and (not self.stats.game_active)): # Start the game.
+            self._start_game()    
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
 
     def _check_keyup_events(self, event):
         """ Respond to key releases
         """
+
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    # Bullet processing
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group.
         """
--- settings.py
+++ settings.py
@@ -18,18 +18,17 @@ class Settings:
         self.ship_limit      = 3
 
         # Bullet settings
-        self.bullet_speed    = 3.5
+        self.bullet_speed    = 3.0 #1.5
         self.bullet_width    = 3
         self.bullet_height   = 15
         self.bullet_color    = (60,60,60)
         self.bullets_allowed = 7
 
         # Alien settings
-        self.alien_speed = 3 #1.5
+        self.alien_speed = 3.5 #1.5
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
 
         # Display settings
-        self.fps = 30
-        
+        self.fps = 30     

f8f8a1d42ac133cb9e9d4bc024782684e5e401df
shulme801 <shulme801@gmail.com>
2021-03-22 17:03:23 -0400

    22 Mar High Score Logic Added

--- alien_invasion.py
+++ alien_invasion.py
@@ -63,6 +63,7 @@ class AlienInvasion:
         self.stats.reset_stats()
         self.stats.game_active = True
         self.sb.prep_score()
+        self.sb.prep_high_score()
 
         # Get rid of any remaining aliens and bullets.
         self.aliens.empty()
@@ -209,6 +210,7 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
--- game_stats.py
+++ game_stats.py
@@ -3,11 +3,16 @@ class GameStats:
 
     def __init__(self, ai_game):
         """ Initialize statistics."""
+
         self.settings = ai_game.settings
         self.reset_stats()
+        
         # Start Alien Invasion in an inactive state.
         self.game_active = False
 
+        # High score should never be reset to 0.
+        self.high_score = 0
+        
     def reset_stats(self):
         """ Initialize statistics that can change during game."""
         self.ships_left = self.settings.ship_limit
